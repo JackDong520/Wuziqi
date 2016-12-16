63bfafba423f15c30059d9b393657ec9ec096bad
JackDong520 <724083996@qq.com>
2016-12-16 17:52:28 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/jack/wuziqi/viewpagerIndicator/ViewPagerIndicator.java
@@ -0,0 +1,17 @@
+package com.example.jack.wuziqi.viewpagerIndicator;
+
+import android.content.Context;
+
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 72408 on 2016/12/16.
+ */
+
+public class ViewPagerIndicator extends LinearLayout {
+    public ViewPagerIndicator(Context context) {
+        super(context);
+    }
+
+}
